4401eeee4c63573c6e4a36e5ea257a4d5fad0956
Azonfox <azonfox@gmail.com>
2024-02-17 23:34:44 +0600

    Font  Layout

--- extract_video_id/build/gen/ru/kx13/extractvidid/R.java
+++ extract_video_id/build/gen/ru/kx13/extractvidid/R.java
@@ -11,23 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int bgwin=0x7f040000;
-        public static final int btn1=0x7f040002;
-        public static final int btn2=0x7f040003;
-        public static final int textColor=0x7f040001;
+        public static final int bgwin=0x7f050000;
+        public static final int btn1=0x7f050002;
+        public static final int btn2=0x7f050003;
+        public static final int textColor=0x7f050001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class font {
+        public static final int fsb=0x7f030000;
+    }
     public static final class id {
-        public static final int button_id=0x7f060001;
-        public static final int button_id2=0x7f060002;
-        public static final int my_text=0x7f060000;
+        public static final int button_id=0x7f070001;
+        public static final int button_id2=0x7f070002;
+        public static final int my_text=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_main=0x7f040000;
     }
     public static final class string {
-        public static final int btn2=0x7f050000;
+        public static final int btn2=0x7f060000;
     }
 }
